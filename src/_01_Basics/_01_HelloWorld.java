2d21f629088d34eafb282a880d790e607c9f293b
omkasera972-ai <omkasera972@gmail.com>
2026-04-12 17:11:56 +0530

    Update _01_HelloWorld.java

--- src/_01_Basics/_01_HelloWorld.java
+++ src/_01_Basics/_01_HelloWorld.java
@@ -1,6 +1,6 @@
 package _01_Basics;
 
-public class HelloWorld {
+public class _01_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World, java mastery");
     }
